2422dc091b2622b31a65e441251a6dc00ad08fd8
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-02-23 23:21:55 +0100

    Basic manipulation of widgets

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich ({Key key}) : super (key: key);
+
+  final theme= FooderlichTheme.dark();
+  @override
+  Widget build(Buildcontext context) {
+
+    return MaterialApp(
+
+      theme: theme,
+      title: 'Fooderlich';
+
+      home: Scaffold(
+        appBar: AppBar(title: Text ('Fooderlich',
+        style: theme.textTheme.headline6),),
+        body: const Center(child: Text('Let\'s get cooking 👨‍🍳',
+        style: theme.textTheme.headline1),),
+      ),
+    );
+  }
+}
