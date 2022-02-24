c48c226f8c62e657420cc0bfd05978d8c33899f2
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-02-24 22:43:00 +0100

    added bottom navbar

--- lib/home.dart
+++ lib/home.dart
@@ -21,6 +21,23 @@ class _HomeState extends State<Home> {
           style: Theme.of(context).textTheme.headline1
         )
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card1'),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2'),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3'),
+        ],
+      ),
       
     );
   }
