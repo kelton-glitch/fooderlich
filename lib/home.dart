0e233f576d1136852ca6269aaf5dfc639c7acbdb
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-02-26 21:33:21 -0600

    attributing different colors to navbard

--- lib/home.dart
+++ lib/home.dart
@@ -1,44 +1,47 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget{
-  const Home ({Key? key}) : super (key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static List<Widget> pages = <Widget>[
+    Container(color: Colors.red),
+    Container(color: Colors.green),
+    Container(color: Colors.blue)
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('Fooderlich', style: Theme.of(context).textTheme.headline6), 
-        ),
-      
-      body: Center (
-        child: Text(
-          'Let\'s get cooking 👨‍🍳',
-          style: Theme.of(context).textTheme.headline1
-        )
+        title: Text('Fooderlich', style: Theme.of(context).textTheme.headline6),
       ),
-
+      body: pages [_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
-
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         items: <BottomNavigationBarItem>[
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card1'),
+              icon: Icon(Icons.card_giftcard), label: 'Card1'),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card2'),
+              icon: Icon(Icons.card_giftcard), label: 'Card2'),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: 'Card3'),
+              icon: Icon(Icons.card_giftcard), label: 'Card3'),
         ],
       ),
-      
     );
   }
 }
