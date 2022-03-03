ff9a4f8c71d4cc41c750510eb2333cac675305a1
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-03-03 20:35:19 +0100

    Setting up the second card and adding image to it

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/card1.dart';
+import 'package:fooderlich/card2.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,7 +14,7 @@ class _HomeState extends State<Home> {
 
   static List<Widget> pages = <Widget>[
     const Card1(),
-    Container(color: Colors.green),
+    const Card2(),
     Container(color: Colors.blue)
   ];
 
