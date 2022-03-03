2dcaec84243d33fee28e3984201ffa62332ff86f
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-03-03 20:05:06 +0100

    adding style to cards

--- lib/card1.dart
+++ lib/card1.dart
@@ -11,7 +11,16 @@ class Card1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        constraints: const BoxConstraints.expand(width: 350, height: 450),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag1.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
