6c9c2732d09ec1f460a55b1cda19a39ebdfdf04a
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-03-03 21:30:32 +0100

    First card

---
+++ lib/card1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+
+  final String category = 'Editor\'s Choice';
+  final String title = 'The Art of Dough';
+  final String description = 'Learn to make the perfect bread';
+  final String chef = 'Kamgo Elton';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(),
+    );
+  }
+}
