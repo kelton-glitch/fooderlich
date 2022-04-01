35214913c2a053fb35c222967054ba8f8dc53343
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-03-31 22:10:30 -0600

    revert to previous mode

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'fooderlich_theme.dart';
-import 'models/models.dart';
 import 'author_card.dart';
 
 class Card2 extends StatelessWidget {
-  final ExploreRecipe recipe;
-
   const Card2({
     Key? key,
-    required this.recipe,
   }) : super(key: key);
 
   @override
